f4d3795660de844d168eab6f4928ef3fb1fd6ae3
Yyax13 <138953949+Yyax13@users.noreply.github.com>
2023-07-14 16:58:50 -0300

    Delete Rvng-tools directory

--- Rvng-tools/Presets/python codes for study/clear.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def limpar_tela():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-# Exemplo de uso
-print("Texto antes de limpar a tela")
-limpar_tela()
-print("Texto após limpar a tela")
--- Rvng-tools/Presets/python codes for study/par-impar.py
+++
@@ -1,8 +0,0 @@
-def verificar_numero_par(numero):
-    if numero % 2 == 0:
-        print("O número é par.")
-    else:
-        print("O número é ímpar.")
-
-numero = input("numero:  ")
-verificar_numero_par(numero)
